9c60244c90120319bce7bb215d0d8a690c2131cd
Daniel Hampikian <danielhampikian@gmail.com>
2022-02-21 13:28:14 -0700

    all

--- random.js
+++ random.js
@@ -1,15 +1,43 @@
 var inputMin;
 var inputMax;
-var
-//write a random number between min and max including both numbers
-//math.floor insures we round down to the nearest integer
+var displayNum;
+
+function randomSpin() {
+    var timesRun = 0;
+    var interval = setInterval(function(){
+    timesRun += 1;
+    if(timesRun === 10){
+        clearInterval(interval);
+    }
+    displayRandom();
+}, 500); 
+}
 
 function displayRandom() {
+    displayNum = document.getElementById('displayNum');
+    let output = parseInt(getRndInteger());
+    console.log(output);
+    displayNum.innerHTML = "Your random number is: " + output;
 
 }
+
+function displayItems(output){
+    for(let i=0; i<output; i++) {
+        var card = document.createElement('div');
+        card.style.backgroundColor = 'red';
+        card.style.height = '200px';
+        card.style.width = '200px';
+        card.style.margin = '20px';
+        card.style.borderRadius = '50%';
+        displayNum.appendChild(card);
+    }
+}
+//write a random number between min and max including both numbers
+//math.floor insures we round down to the nearest integer
 function getRndInteger() {
-    inputMin = document.getElementById('minInput');
-    inputMax = document.getElementById('maxOutput');
+    inputMin = parseInt(document.getElementById('minInput').value);
+    inputMax = parseInt(document.getElementById('maxInput').value);
+    console.log(inputMin);
     return Math.floor(Math.random() * (inputMax - inputMin + 1) ) + inputMin;
 }
 //returns between 0 and max including 0 and max, and including decimals
